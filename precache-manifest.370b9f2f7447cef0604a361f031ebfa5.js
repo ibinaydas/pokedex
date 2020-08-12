5f6e06f777120b84c89fe34d67d09329798f4d7f
Binay Das <ibinaydas@gmail.com>
2020-08-12 14:12:46 +0530

    Updates

---
+++ precache-manifest.370b9f2f7447cef0604a361f031ebfa5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2f22b66a23f9bd35bde19d522821519",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "c5d020a951fb64b555f4",
+    "url": "/pokedex/static/css/main.7ae7761e.chunk.css"
+  },
+  {
+    "revision": "1203cbcd5b0983cb03c7",
+    "url": "/pokedex/static/js/2.c423f07d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.c423f07d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5d020a951fb64b555f4",
+    "url": "/pokedex/static/js/main.ff45f7a6.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  }
+]);
